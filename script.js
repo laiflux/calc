6c286b10dbebbbbed6ec744d979f4e7aa514d019
lai <255363213+laiflux@users.noreply.github.com>
2026-02-18 13:16:19 +0530

    Add variables for buttons

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+
+let numX;
+let numY;
+let op;
+const clear = document.getElementById('clear');
+const equal = document.getElementById('equal');
+const signAdd = document.getElementById('add');
+const signSubtract = document.getElementById('subtract');
+const signMultiply = document.getElementById('multiply');
+const signDivide = document.getElementById('divide');
+const decimalPoint = document.getElementById('point');
+const zero = document.getElementById('zero');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+
 function add(num1, num2) {
     return num1 + num2 
 }
@@ -13,10 +35,6 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2
 }
-
-let numX;
-let numY;
-let op;
 
 function operate(num1, num2, op) {
     switch (op) {
@@ -34,3 +52,4 @@ function operate(num1, num2, op) {
             break;
     }
 }
+
